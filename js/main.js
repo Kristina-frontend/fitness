c0232938515a2873404f03a0b4fd44f27d88b172
Kristina-frontend <120197972+Kristina-frontend@users.noreply.github.com>
2022-12-09 16:46:27 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $(".work-btn").click(function(){
+        $(".work-btn").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".header-btn").click(function(){
+        $(".header-btn").removeClass("active");
+        $(this).addClass("active");
+    })
+
+    $(".tab-title").click(function(){
+        $(".tab-title").removeClass("active");
+        $(".tab-block").removeClass("active");
+        $(".tab-content").slideUp();
+        $(this).addClass("active");
+        $(this).parent().addClass("active");
+        $(this).next().slideDown();
+    })
+
+    // slider
+    $(".sliders").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,   
+        autoplay: true,   
+    })
+
+    // stars
+    $(".my-rating").starRating({
+        initialRating: 5,
+        strokeColor: '#FDBC64',
+        strokeWidth: 10,
+        starSize: 25
+      
+    });
+    
+
+    $(".burger-menu").click(function(){
+        $(".burger-menu").toggleClass("active");
+        $(".nav").toggleClass("active");
+        $(".menu").toggleClass("active");
+    })
+
+});
